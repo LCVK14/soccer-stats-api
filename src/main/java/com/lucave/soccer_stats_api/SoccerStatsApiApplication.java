7e256708029f6165a42ba8baea9b94179c250248
Lucas Cajamarca Verdugo <lucasverdugo@outlook.com>
2025-10-17 18:12:36 -0400

    Add component scanning to detect controllers in com.lucave

--- src/main/java/com/lucave/soccer_stats_api/SoccerStatsApiApplication.java
+++ src/main/java/com/lucave/soccer_stats_api/SoccerStatsApiApplication.java
@@ -2,9 +2,10 @@ package com.lucave.soccer_stats_api;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan(basePackages = "com.lucave")
 public class SoccerStatsApiApplication {
 
 	public static void main(String[] args) {
