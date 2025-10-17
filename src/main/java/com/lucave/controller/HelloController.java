cd5b4259cb413af6b7d6d435bfac52892916e696
Lucas Cajamarca Verdugo <lucasverdugo@outlook.com>
2025-10-17 17:54:45 -0400

    Creating a test controller

---
+++ src/main/java/com/lucave/controller/HelloController.java
@@ -0,0 +1,13 @@
+package com.lucave.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+    
+    @GetMapping("/hello")
+    public String helloWorld() {
+        return "Hello World Spring Boot!";
+    }
+}
